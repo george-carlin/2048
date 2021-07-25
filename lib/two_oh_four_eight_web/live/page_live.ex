c21125f843a604cabeec72669658e65716225cd7
George Millo <george@millo.me>
2021-07-25 11:56:22 +0100

    update grid via liveview

--- lib/two_oh_four_eight_web/live/page_live.ex
+++ lib/two_oh_four_eight_web/live/page_live.ex
@@ -1,17 +1,30 @@
 defmodule TwoOhFourEightWeb.PageLive do
   use TwoOhFourEightWeb, :live_view
 
+  @arrow_keys ~w[ArrowUp ArrowDown ArrowLeft ArrowRight]
+
   alias TwoOhFourEight.Game.Grid
 
   @impl true
   def mount(_params, _session, socket) do
     grid = Grid.new() |> Grid.add_random(2)
-    {:ok, assign(socket, grid: grid, key: "")}
+    {:ok, assign(socket, grid: grid)}
   end
 
   @impl true
-  def handle_event("keydown", %{"key" => key}, socket) do
-    {:noreply, assign(socket, key: key)}
+  def handle_event("keydown", %{"key" => key}, socket) when key in @arrow_keys do
+    direction = case key do
+      "ArrowUp" -> :up
+      "ArrowDown" -> :down
+      "ArrowRight" -> :right
+      "ArrowLeft" -> :left
+    end
+
+    grid = socket.assigns.grid
+           |> Grid.shift(direction)
+           |> Grid.add_random(1)
+
+    {:noreply, assign(socket, grid: grid)}
   end
 
   #@impl true
